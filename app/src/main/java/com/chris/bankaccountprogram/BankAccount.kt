a6647545b7ab7c599abb2fab15ef0da7a6f194c2
Chriztey <CHRISTIETEYDADDY@GMAIL.COM>
2024-03-03 20:24:01 +0700

    implement function
    withdraw, deposit, transaction list, balance

--- app/src/main/java/com/chris/bankaccountprogram/BankAccount.kt
+++ app/src/main/java/com/chris/bankaccountprogram/BankAccount.kt
@@ -2,16 +2,30 @@ package com.chris.bankaccountprogram
 
 class BankAccount (var accountHolder : String, var balance : Double) {
 
-    private val transactionHistory = mutableListOf<String>()
+    private var transactionHistory = mutableListOf<String>()
 
     fun deposit (amount : Double) {
+        balance += amount
+        transactionHistory.add("$accountHolder deposited $$amount")
 
     }
 
     fun withdraw (amount: Double) {
+        if (amount <= balance) {
+            balance -= amount
+            transactionHistory.add(("$accountHolder withdraw $$amount"))}
+        else println("Balance not enough")
+
+
 
     }
 
-    fun displayTransactionHistory(){}
+    fun displayTransactionHistory(){
+        println("Transaction History from $accountHolder's Account")
+
+        for (transaction in transactionHistory) {
+            println(transaction)
+        }
+    }
 
 }
--- app/src/main/java/com/chris/bankaccountprogram/BankMain.kt
+++ app/src/main/java/com/chris/bankaccountprogram/BankMain.kt
@@ -5,4 +5,12 @@ fun main () {
     val chrisBankAccount = BankAccount("Chris", 100.00)
     println(chrisBankAccount.accountHolder)
 
+    chrisBankAccount.deposit(200.00)
+    chrisBankAccount.withdraw(amount = 1500.00)
+    chrisBankAccount.deposit(300.00)
+    chrisBankAccount.withdraw(100.00)
+    chrisBankAccount.displayTransactionHistory()
+
+    println("total ${chrisBankAccount.accountHolder}'s balance is ${chrisBankAccount.balance}")
+
 }
